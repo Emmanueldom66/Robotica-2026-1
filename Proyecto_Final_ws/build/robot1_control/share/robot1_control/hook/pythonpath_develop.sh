17500429e6449eecc618d7605f957fdd4f97e630
Emmanueldom66 <emmanueldom007@outlook.com>
2025-11-30 16:50:19 -0600

    Workspace Created.

---
+++ Proyecto_Final_ws/build/robot1_control/share/robot1_control/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/robousr/Desktop/Robotica_Semestre-2026-1/Proyecto_Final_ws/build/robot1_control"
---
+++ Proyecto_Final_ws/build/robot1_description/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/robousr/Desktop/Robotica_Semestre-2026-1/Proyecto_Final_ws/install/robot1_description'
